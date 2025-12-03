a5371f8d1fed4b7045dd988f857d2fdb306145fc
gwtt0878 <gwtt0878@gmail.com>
2025-12-03 21:59:20 +0900

    minor: Options 메서드 허용

--- dagachi_be/src/main/java/com/gwtt/dagachi/config/SecurityConfig.java
+++ dagachi_be/src/main/java/com/gwtt/dagachi/config/SecurityConfig.java
@@ -18,6 +18,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.http.HttpMethod;
 
 @Configuration
 @EnableWebSecurity
@@ -37,7 +38,9 @@ public class SecurityConfig {
         .sessionManagement(
             session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
         .authorizeHttpRequests(
-            auth -> auth.requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
+            auth -> auth
+            .requestMatchers(HttpMethod.OPTIONS,"/**").permitAll()
+            .requestMatchers("/api/auth/**").permitAll().anyRequest().authenticated())
         .exceptionHandling(
             exception -> exception.authenticationEntryPoint(jwtAuthenticationEntryPoint))
         .addFilterBefore(jwtAuthenticationFilter, UsernamePasswordAuthenticationFilter.class);
